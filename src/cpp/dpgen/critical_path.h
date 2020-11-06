74994c039eece3d0f5fd91a131c78e9e932b7d67
dpax182 <73152055+dpax182@users.noreply.github.com>
2020-11-06 14:10:02 -0600

    Update critical_path.h

--- src/cpp/dpgen/critical_path.h
+++ src/cpp/dpgen/critical_path.h
@@ -1,7 +1,6 @@
 #ifndef CRITICAL_PATH_H
 #define CRITICAL_PATH_H
 
-
 #include "data_list.h"
 #include "data_type.h"
 
@@ -10,6 +9,5 @@ int TopSort(data_list Graph, data_list List, data_type vertex);
 int TopSortVisit(data_list Graph, data_list List, data_type uVertex);
 
 int LongestPath(data_list Graph);
-
 
 #endif
