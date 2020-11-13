0d03ca950f11ade3d2fb70bcdcd30f7f0bc414e1
samuelcomeau6 <ssamuelcomeau@gmail.com>
2020-11-13 08:44:11 -0600

    Changed cmake version

--- submit.sh
+++ submit.sh
@@ -7,6 +7,6 @@ cp ./src/cpp/dpgen/*.h $subfolder/src/
 cp ./src/cpp/dpgen/*.vh $subfolder/src/
 cp ./src/cpp/dpgen/CMakeLists.txt $subfolder/src/
 cp ./src/cpp/dpgen/CMakeLists.txt $subfolder/
-printf "cmake_minimum_required(VERSION 3.17)\n" > $subfolder/CMakeLists.txt
+printf "cmake_minimum_required(VERSION 3.15)\n" > $subfolder/CMakeLists.txt
 printf "project(dpgen)\n" >> $subfolder/CMakeLists.txt
 printf "add_subdirectory(src)" >> $subfolder/CMakeLists.txt
