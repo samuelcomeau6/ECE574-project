ed9ef80ba01dd58119b18cea75861c0ad06a3a2f
samuelcomeau6 <ssamuelcomeau@gmail.com>
2020-11-13 07:36:33 -0600

    Created submit.sh script for use in github action

--- .github/workflows/submit.sh
+++ .github/workflows/submit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd ../../
-dir = $PWD
-subfolder = "./submission/NetID1_NetID2_NetID3_dpgen"
-mkdir $subfolder
+dir=$PWD
+subfolder="./submission/NetID1_NetID2_NetID3_dpgen"
+rm -rf $subfolder
+mkdir -p $subfolder/src
 cp ./src/cpp/dpgen/*.cpp $subfolder/src/
 cp ./src/cpp/dpgen/*.h $subfolder/src/
+cp ./src/cpp/dpgen/*.vh $subfolder/src/
 cp ./src/cpp/dpgen/CMakeLists.txt $subfolder/src/
+cp ./src/cpp/dpgen/CMakeLists.txt $subfolder/
+printf "add_subdirectory(src)" > $subfolder/CMakeLists.txt
